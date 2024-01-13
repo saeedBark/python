0e2f14fd40e0ea1d12dc7c81830df3aaf84cd412
saeed_bark <saeedbark96@gmail.com>
2024-01-13 10:41:20 +0000

    first python tp

---
+++ python_tp_1.py
@@ -0,0 +1,26 @@
+# def entiers(i,j):
+#     for entier in range(i,j+1):
+#         print(entier,end="-")
+
+    
+
+# entiers(1,5) 
+
+
+# a = [0,1,'abc',4.5,'de',6]
+# print(a[1:5])
+
+for x in range(11):
+    if(x*x <= 50):
+        print(x)
+
+
+
+l =list( range(11))
+
+ls = list('abcde')
+
+print(ls)
+
+tup = (1,2,6)
+tup[3] = 5 
